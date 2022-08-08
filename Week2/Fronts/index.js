6225f8faa96d245d4adb3b47e1dfe6d65cb753e7
dibranimargarita <dibrani@pupil.ch>
2022-08-08 12:49:35 +0200

    Week 2 - Fronts

---
+++ Week2/Fronts/index.js
@@ -0,0 +1,33 @@
+class SLLNode {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    addFront(value) {
+        var newNode = new SLLNode(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        return this.head;
+    }
+
+    removeFront() {
+        if(this.head == null) {
+            return this.head;
+        }
+        var removedNode = this.head;
+        this.head = removedNode.next;
+        removedNode.next = null;
+        return this.head;
+    }
+
+    front() {
+        return this.head ? this.head.value : null; 
+    }
+}
